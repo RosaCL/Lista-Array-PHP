53708c3865798dc56cbb40925cb86ef61da50ef3
Rosa <costurezati@gmail.com>
2025-03-14 19:35:36 -0300

    .

--- public/ex001.php
+++ public/ex001.php
@@ -7,77 +7,66 @@
     <link rel="stylesheet" href="../resources/css/style.css">
 </head>
 <body>
-    <?php 
-        $nome=$_POST['nome'];
-        $nota=$_POST['nota'];
-        $alunos =[];
-        $soma=0;
-        $maiorNota=-1;
-        $alunoMaiornota="";
-    ?>
     <main>
         <section class="pergunta">
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-        <?php for ($i=0; $i<10; $i++):?>
-                        <div class="form-content" <?php echo $i;?>>
-                            <fieldset><legend><h3 class="title" >Dados Aluno <?php echo $i +1;?></h3></legend>
-                            <label class="dado-label" for="nome">Nome:</label><br>
-                            <input class="dado-input" type="text"  id="nome" name="nome" value="<?=$nome?>"><br>
-                            <label  class="dado-label" for="nota">Nota</label><br>
-                            <input class="dado-input" type="number"  id="nota" name="nota" step="0.01" value="<?=$nota?>">     
+            <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+                <?php for ($i = 0; $i < 10; $i++): ?>
+                    <div class="form-content" id="form-content-<?php echo $i;?>">
+                        <fieldset><legend><h3 class="title">Dados Aluno <?php echo $i + 1; ?></h3></legend>
+                            <label class="dado-label" for="nome<?php echo $i;?>">Nome:</label><br>
+                            <input class="dado-input" type="text" id="nome<?php echo $i;?>" name="nome[]"><br>
+                            <label class="dado-label" for="nota<?php echo $i;?>">Nota</label><br>
+                            <input class="dado-input" type="number" id="nota<?php echo $i;?>" name="nota[]" step="0.01">
                         </fieldset>
-                        </div>
-                        <?php endfor;?>
-                        <div>                            
-                            <input class="enviar" type="submit" value="Enviar">
-                        </div>
-            
-                </form>
-
+                    </div>
+                <?php endfor; ?>
+                <div>
+                    <input class="enviar" type="submit" value="Enviar">
+                </div>
+            </form>
         </section>
-        
     </main>
     <section class="resposta">
         <div class="form-content">
-            <fieldset><legend> <h1 class="title">Dados Recebidos</h1></legend>
-            <div class="res">
-            <?php       
-                if ($_SERVER["REQUEST_METHOD"] === 'POST' ){
-                            
-                        for ($i = 0; $i < 10; $i++){
-                            $alunos[] =[
-                                'nome' => $nome[$i],
-                                'nota' => $nota[$i]
+            <fieldset><legend><h1 class="title">Dados Recebidos</h1></legend>
+                <div class="res">
+                    <?php
+                    if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+                        $nomes = $_POST['nome'];
+                        $notas = $_POST['nota'];
+                        $alunos = [];
+                        $soma = 0;
+                        $maiorNota = -1;
+                        $alunoMaiorNota = "";
+
+                        for ($i = 0; $i < 10; $i++) {
+                            $alunos[] = [
+                                'nome' => $nomes[$i],
+                                'nota' => $notas[$i]
                             ];
-            
-            
-                        $soma+=$nota[$i];
-            
-                        if ($nota[$i]>$maiorNota){
-                            $maiorNota=$nota[$i];
-                            $alunoMaiornota=$nome[$i];
+
+                            $soma += $notas[$i];
+
+                            if ($notas[$i] > $maiorNota) {
+                                $maiorNota = $notas[$i];
+                                $alunoMaiorNota = $nomes[$i];
+                            }
                         }
+
+                        $media = $soma / 10;
+
+                        echo "<ul>
+                                <li><strong>Média da turma: </strong> $media</li>
+                                <li><strong>Maior nota: </strong> $alunoMaiorNota</li>
+                            </ul>";
                     }
-                        $media=$soma/$i;
-                    }        
-                    echo "<ul>
-                            <li><strong>Média da turma: </strong> $media</li>
-                            <li><strong>Maior nota: </strong>$alunoMaiornota</li>
-                        </ul>";
-            ?>   
-            </div>
-            </fieldset>  
+                    ?>
+                </div>
+            </fieldset>
         </div>
-    
-                    
-
-        
-
-
     </section>
     <footer>
-            <a href="https://github.com/RosaCL" target="_blank"><img src="../public/img/costurezaa.png" alt=""></a>
-        </footer>
-    
+        <a href="https://github.com/RosaCL" target="_blank"><img src="../public/img/costurezaa.png" alt=""></a>
+    </footer>
 </body>
 </html>
